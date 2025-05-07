c05ce2e024c1a226a225195edd897b7a4bee0817
Saulo217 <78312710+Saulo217@users.noreply.github.com>
2025-05-06 22:54:56 -0300

    clean

--- use_trained_model.py
+++ use_trained_model.py
@@ -2,40 +2,23 @@ import tensorflow as tf
 from tensorflow.keras.preprocessing import image
 import numpy as np
 
-# Load the trained model
-model = tf.keras.models.load_model("./model/classification_model.h5")
+model = tf.keras.models.load_model("./model/classification_model.keras")
 
-# Define the target image size (must match the training image size)
 img_width, img_height = 100, 100
 
 
 def classify_image(image_path):
-    """
-    Loads an image, preprocesses it, and uses the trained model to predict its class.
-
-    Args:
-        image_path (str): The path to the image file.
-
-    Returns:
-        str: The predicted class label ('class_0' or 'class_1') and the probability.
-    """
     try:
-        # Load the image
         img = image.load_img(image_path, target_size=(img_width, img_height))
 
-        # Convert the image to a numpy array
         img_array = image.img_to_array(img)
 
-        # Expand the dimensions to create a batch of size 1
         img_array = np.expand_dims(img_array, axis=0)
 
-        # Normalize the image (as done during training)
         img_array /= 255.0
 
-        # Make the prediction
         prediction = model.predict(img_array)
 
-        # Interpret the prediction (assuming binary classification)
         if prediction[0] > 0.5:
             predicted_class = "class_1"
             probability = prediction[0][0]
@@ -51,9 +34,9 @@ def classify_image(image_path):
         return f"An error occurred: {e}"
 
 
-# Example usage:
-print(classify_image("./to_classify/fork.png"))
-print(classify_image("./to_classify/spoon.jpg"))
-print(classify_image("./to_classify/fork1.jpg"))
-print(classify_image("./to_classify/knife.jpg"))
-print(classify_image("./to_classify/trident_001.jpg"))
+print(classify_image("./to_classify/bg.jpg"))
+print(classify_image("./to_classify/tom.jpg"))
+print(classify_image("./to_classify/pa.png"))
+print(classify_image("./to_classify/pedra.jpg"))
+print(classify_image("./to_classify/pedra_es.jpg"))
+print(classify_image("./to_classify/lim.jpg"))
