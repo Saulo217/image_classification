7fa8f2ba33acbeac23208fb4a112e9c5f2246ebd
saulo <78312710+Saulo217@users.noreply.github.com>
2025-05-11 21:36:42 -0300

    fix: receives file from api

--- use_trained_model.py
+++ use_trained_model.py
@@ -1,33 +1,31 @@
+import numpy as np
 import tensorflow as tf
 from tensorflow.keras.preprocessing import image
-import numpy as np
-
-model = tf.keras.models.load_model("./model/classification_model.keras")
 
 img_width, img_height = 100, 100
 
-
-def classify_image(image_path):
+def classify_image(file):
+    model = tf.keras.models.load_model("./model/classification_model.keras")
     try:
-        img = image.load_img(image_path, target_size=(img_width, img_height))
-
+        img = Image.open(BytesIO(file.read()))
+        img = img.resize((img_width, img_height))
         img_array = image.img_to_array(img)
-
         img_array = np.expand_dims(img_array, axis=0)
-
         img_array /= 255.0
 
         prediction = model.predict(img_array)
 
-        prob_class_0 = prediction[0][0]  # Probability of class_0
-        prob_class_1 = prediction[0][1]  # Probability of class_1
+        prob_class_0 = prediction[0][0]
+        prob_class_1 = prediction[0][1]
 
-        result = f"Class_0: {'Yes' if prob_class_0 > 0.5 else 'No'}, Probability: {prob_class_0:.4f}\n"
-        result += f"Class_1: {'Yes' if prob_class_1 > 0.5 else 'No'}, Probability: {prob_class_1:.4f}"
+        result = {
+            "Class_0": "Yes" if prob_class_0 > 0.5 else "No",
+            "Probability_Class_0": float(prob_class_0),
+            "Class_1": "Yes" if prob_class_1 > 0.5 else "No",
+            "Probability_Class_1": float(prob_class_1)
+        }
 
-        print(result)
         pred = ""
-
         if prob_class_0 == 1 and prob_class_1 == 0:
             pred = "fruta"
         elif prob_class_1 == 1 and prob_class_0 == 0:
@@ -35,12 +33,7 @@ def classify_image(image_path):
         else:
             pred = "outros"
 
-        return pred
+        return result, pred
 
-    except FileNotFoundError:
-        return f"Error: Image not found at {image_path}"
     except Exception as e:
-        return f"An error occurred: {e}"
-
-
-classify_image("./to_classify/goku.jpg")
+        return {"error": str(e)}, None
